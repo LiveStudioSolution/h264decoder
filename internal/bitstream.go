6e5b8999f90af5ab6a157e319be1cf55e2580371
Flonly <liuzhenhua_sf@163.com>
2019-07-13 22:46:54 +0800

    init bitstream

--- internal/bitstream.go
+++ internal/bitstream.go
@@ -1,8 +1,21 @@
 package internal
 
-import "io"
+import (
+	"bufio"
+	"io"
+)
 
+var (
+	annexBSpliter1 = []byte{0, 0, 1}
+	annexBSpliter2 = []byte{0, 0, 0, 1}
+)
+
+func init() {
+
+}
+
+// BitStream H264 bit stream, contains nalu
 type BitStream struct {
-	src    io.Reader
-	buffer bufio
+	src     io.Reader
+	scanner bufio.Scanner
 }
